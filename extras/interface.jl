6416b61679835d0efb7ea5efd4d4b95d742e4410
Patrick O'Leary <patrick.oleary@gmail.com>
2012-04-29 20:27:03 -0500

    Use explicit type variable for flexibility

--- extras/interface.jl
+++ extras/interface.jl
@@ -6,18 +6,26 @@ end
 type Interface
     name::String
     sigs::Vector{Signature}
+    tvar::Symbol
 end
+
+subst_type(sig, tpe, tvar) = tuple(map(t -> t == tvar ? tpe : eval(t), sig.args)...)
 
 function check_interface(iface::Interface, tpe::Type)
     hasmethods = falses(size(iface.sigs))
     for (sig, i) in enumerate(iface.sigs)
-        # this is unfortunate--can't check the symbol table
-        try
-            hasmethods[i] = method_exists(eval(sig.method), tuple(tpe, sig.args...))
-        catch err
-            # this is hokey on top of unfortunate
-            if isa(match(r" not defined$", err.msg), Nothing)
-                throw(err)
+        # first put our new type in place
+        args = subst_type(sig.args, tpe, iface.tvar)
+        method = eval(sig.method)
+        if isa(method, Function)
+            # this is unfortunate--can't check the symbol table
+            try
+                hasmethods[i] = method_exists(eval(sig.method), args)
+            catch err
+                # this is hokey on top of unfortunate
+                if isa(match(r" not defined$", err.msg), Nothing)
+                    throw(err)
+                end
             end
         end
     end
@@ -27,12 +35,12 @@ end
 function verify_interface(iface::Interface, tpe::Type)
     hasmethods = check_interface(iface, tpe)
     for sig in iface.sigs[!hasmethods]
-        args = tuple(tpe, sig.args...)
-        println("Interface $(iface.name): Type $tpe is missing method $(sig.method) with argument types $args")
+        args = subst_type(sig.args, tpe, iface.tvar)
+        println("Interface $(iface.name): Type $tpe is missing method $(sig.method)$args")
     end
     all(hasmethods)
 end
 
-macro interface(name, sigs)
-    :($name = Interface($(string(name)), $sigs))
+macro interface(name, tvar, sigs)
+    :($name = Interface($(string(name)), $sigs, $tvar))
 end
