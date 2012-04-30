1af850d36ee16267589d20b5a80394f578d1139f
Patrick O'Leary <patrick.oleary@gmail.com>
2012-04-29 21:22:32 -0500

    removed Signature type, using a tuple instead

--- extras/interface.jl
+++ extras/interface.jl
@@ -1,11 +1,6 @@
-type Signature
-    method::Symbol
-    args
-end
-
 type Interface
     name::String
-    sigs::Vector{Signature}
+    sigs::Vector
     tvar
 end
 
@@ -15,12 +10,12 @@ function check_interface(iface::Interface, tpe::Type)
     hasmethods = falses(size(iface.sigs))
     for (sig, i) in enumerate(iface.sigs)
         # first put our new type in place
-        args = subst_type(sig.args, tpe, iface.tvar)
-        method = eval(sig.method)
+        args = subst_type(sig[2], tpe, iface.tvar)
+        method = eval(sig[1])
         if isa(method, Function)
             # this is unfortunate--can't check the symbol table
             try
-                hasmethods[i] = method_exists(eval(sig.method), args)
+                hasmethods[i] = method_exists(method, args)
             catch err
                 # this is hokey on top of unfortunate
                 if isa(match(r" not defined$", err.msg), Nothing)
@@ -35,8 +30,8 @@ end
 function verify_interface(iface::Interface, tpe::Type)
     hasmethods = check_interface(iface, tpe)
     for sig in iface.sigs[!hasmethods]
-        args = subst_type(sig.args, tpe, iface.tvar)
-        println("Interface $(iface.name): Type $tpe is missing method $(sig.method)$args")
+        args = subst_type(sig[2], tpe, iface.tvar)
+        println("Interface $(iface.name): Type $tpe is missing method $(sig[1])$args")
     end
     all(hasmethods)
 end
